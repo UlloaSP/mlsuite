f1a606101fac8d1a0c311ffd86ac8553379fb66c
Pablo Ulloa Santín <pablo.ulloa.santin@udc.es>
2026-04-22 10:39:39 +0200

    HTTPS CA CERT

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,38 +7,50 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite-plus";
 
-export default defineConfig({
-  fmt: {},
-  lint: { "options": { "typeAware": true, "typeCheck": true } },
-  plugins: [
-    react({
-      plugins: [],
-      disableOxcRecommendation: true,
-    }),
-    tailwindcss(),
-  ],
-  build: {
-    outDir: "./dist",
-    emptyOutDir: false,
-    rolldownOptions: {
-      output: {
-        codeSplitting: {
-          groups: [
-            {
-              name: "monaco",
-              test: /node_modules[\\/](?:@monaco-editor[\\/]react|monaco-editor)(?:[\\/]|$)/,
-            },
-            {
-              name: "typescript",
-              test: /node_modules[\\/]typescript(?:[\\/]|$)/,
-            },
-            {
-              name: "react",
-              test: /node_modules[\\/](?:react|react-dom)(?:[\\/]|$)/,
-            }
-          ],
+export default defineConfig(({ command }) => {
+  const isServe = command === "serve";
+
+  return {
+    fmt: {},
+    lint: { "options": { "typeAware": true, "typeCheck": true } },
+    server: isServe
+      ? {
+        https: {
+          key: ".cert/localhost-key.pem",
+          cert: ".cert/localhost.pem",
+        },
+      }
+      : undefined,
+    plugins: [
+      react({
+        plugins: [],
+        disableOxcRecommendation: true,
+      }),
+      tailwindcss(),
+    ],
+    build: {
+      outDir: "./dist",
+      emptyOutDir: false,
+      rolldownOptions: {
+        output: {
+          codeSplitting: {
+            groups: [
+              {
+                name: "monaco",
+                test: /node_modules[\\/](?:@monaco-editor[\\/]react|monaco-editor)(?:[\\/]|$)/,
+              },
+              {
+                name: "typescript",
+                test: /node_modules[\\/]typescript(?:[\\/]|$)/,
+              },
+              {
+                name: "react",
+                test: /node_modules[\\/](?:react|react-dom)(?:[\\/]|$)/,
+              }
+            ],
+          },
         },
       },
     },
-  },
+  };
 });
